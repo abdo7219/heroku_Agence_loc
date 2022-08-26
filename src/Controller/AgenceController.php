9516f5d3f7a4fae40eb06cb74987fa9f680dbebd
Abderrahim <abdo7219@hotmail.com>
2022-08-26 21:57:33 +0200

    bug3

--- src/Controller/AgenceController.php
+++ src/Controller/AgenceController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 
-use app\Entity\Vehicule;
-use app\Entity\User;
-use App\Form\VehiculeType;
+// use app\Entity\Vehicule;
+// use app\Entity\User;
+// use App\Form\VehiculeType;
 use App\Repository\VehiculeRepository;
-use App\Repository\CommandeRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
+// use App\Repository\CommandeRepository;
+// use Doctrine\ORM\EntityManagerInterface;
+// use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
